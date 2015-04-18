a21f280415d95cfa679833483357863768dce31c
Kevin Thompson <kevin.david.thompson@gmail.com>
2015-04-18 10:19:47 -0700

    adds graphs to prediction app

--- server.R
+++ server.R
@@ -2,23 +2,120 @@ library(shiny)
 library(data.table)
 library(tm)
 library(RWeka)
+library(ggplot2)
+library(dplyr)
+options(mc.cores=1)
 
 load("lookup.Rda")
 load("fourGrams.Rda")
-kevTokenizer <- function(x) NGramTokenizer(x, Weka_control(min=3, max=3))
+load("triGrams.Rda")
+load("biGrams.Rda")
 
-predict <- function(inString) {
+herokuPurple <- "#3C378B"
+
+cleanText <- function(inString) {
   incorp <- Corpus(VectorSource(inString))
   incorp <- tm_map(incorp, content_transformer(tolower))
   incorp <- tm_map(incorp, removePunctuation)
   incorp <- tm_map(incorp, removeNumbers)
-  ngrams <- kevTokenizer(incorp$content[[1]])
+  return(incorp$content[[1]][[1]])
+}
+
+predict2 <- function(inString) {
+  kevTokenizer <- function(x) NGramTokenizer(x, Weka_control(min=1, max=1))
+  ngrams <- kevTokenizer(cleanText(inString))
   lastngram <- unlist(strsplit(ngrams[length(ngrams)], " "))
-  # prediction <- lookup[list(lastngram[1], lastngram[2], lastngram[3])]$o1
+  prediction <- triGrams[list(lastngram[1])]$o1
+  prediction <- c(prediction, c("the","to","and","a","of"))
+  prediction <- prediction[!is.na(prediction)]
+  return(prediction[1:5])
+}
+
+predict3 <- function(inString) {
+  kevTokenizer <- function(x) NGramTokenizer(x, Weka_control(min=2, max=2))
+  ngrams <- kevTokenizer(cleanText(inString))
+  lastngram <- unlist(strsplit(ngrams[length(ngrams)], " "))
+  prediction <- triGrams[list(lastngram[1], lastngram[2])]$o1
+  if(is.na(prediction)) { return(predict2(inString))}
+  if(length(prediction) <5) {prediction <- c(prediction, predict2(inString))}
+  return(prediction[1:5])
+}
+predict4 <- function(inString) {
+  kevTokenizer <- function(x) NGramTokenizer(x, Weka_control(min=3, max=3))
+  ngrams <- kevTokenizer(cleanText(inString))
+  lastngram <- unlist(strsplit(ngrams[length(ngrams)], " "))
   prediction <- fourGrams[list(lastngram[1], lastngram[2], lastngram[3])]$o1
-  return(prediction)
+  if(is.na(prediction)) { return(predict3(inString)) }
+  if(length(prediction) < 5) {prediction <- c(prediction, predict3(inString))}
+  return(prediction[1:5])
+}
+
+predict <- function(inString) {
+  inStringLength <- length(unlist(strsplit(inString, " ")))
+  if(inStringLength == 0) {
+    return(c("I","The","I'm","You","Thanks"))
+  }
+  if(inStringLength == 1) {
+    return(predict2(inString))
+  }
+  
+  if(inStringLength == 2) {
+    return(predict3(inString))
+  }
+  
+  if(inStringLength > 2) {
+    return(predict4(inString))
+  }
 }
 
 shinyServer(function(input, output) {
   output$text1 <- renderText({ predict(input$inputText) })
+  output$bigrams <- renderPlot({
+    clean <- cleanText(input$inputText)
+    words <- unlist(strsplit(clean, " "))
+    if(length(words) >= 1) {
+      lastword <- tolower(words[length(words)])
+      results <- biGrams[list(lastword)]
+      if(!is.na(results$o1[1])) {
+        results$o1 <- factor(results$o1, levels = arrange(results, n)$o1)
+        biplot <- ggplot(results, aes(x=o1, y=n)) + geom_bar(stat="identity", fill=herokuPurple) +
+          theme_bw() + coord_flip() +
+          labs(title="Frequent words based on last word in the input text", x="Prediction", y="Count")
+        return(biplot)  
+      }
+    }
+  })
+  output$trigrams <- renderPlot({
+    clean <- cleanText(input$inputText)
+    words <- unlist(strsplit(clean, " "))
+    if(length(words) >=2) {
+      lastwords <- tolower(words[length(words)-1:length(words)])
+      results <- triGrams[list(words[1], words[2])]
+      if(!is.na(results$o1[1])) {
+        results$o1 <- factor(results$o1, levels = arrange(results, n)$o1)
+        triplot <- ggplot(results, aes(x=o1, y=n)) + geom_bar(stat="identity", fill=herokuPurple) +
+          theme_bw() + coord_flip() +
+          labs(title="Frequent words based on last two words in the input text", x="Prediction", y="Count")
+        return(triplot)
+      }
+      
+    }
+  })
+  
+  output$fourgrams <- renderPlot({
+    clean <- cleanText(input$inputText)
+    words <- unlist(strsplit(clean, " "))
+    if(length(words) >=3) {
+      lastwords <- tolower(words[length(words)-2:length(words)])
+      results <- triGrams[list(words[1], words[2], words[3])]
+      if(!is.na(results$o1[1])) {
+        results$o1 <- factor(results$o1, levels = arrange(results, n)$o1)
+        triplot <- ggplot(results, aes(x=o1, y=n)) + geom_bar(stat="identity", fill=herokuPurple) +
+          theme_bw() + coord_flip() +
+          labs(title="Frequent words based on last three words in the input text", x="Prediction", y="Count")
+        return(triplot)
+      }
+    }
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -6,5 +6,13 @@ shinyUI(fluidPage(
   p("Enter a message into the text box below and the application will attempt to predict the next word."),
   textInput("inputText", label= h3("Input text")),
   h3("Suggested words"),
-  textOutput("text1")
+  textOutput("text1"),
+  HTML("<hr/>"),
+  h3("Explanation"),
+  p("The input text is broken up into 3-grams and then the last 3-gram in the input text is checked against a data.table of 4-grams. The last word for the 4-grams is returned."),
+  p("The program tries to return 5 suggestions. If there are not five words returned then it will break the input text into bigrams and return the results from a table of trigrams"),
+  p("As a last resort, the app will return the five most frequently seen words in the english language"),
+  plotOutput("bigrams"),
+  plotOutput("trigrams"),
+  plotOutput("fourgrams")
 ))
